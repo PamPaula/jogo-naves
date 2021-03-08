4410e55ee88b323bfb8eaf7e0dc2d62698cf9569
PamPaula <pamela.dpaulas@gmail.com>
2021-03-08 17:26:56 -0300

    criando arquivo javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+//iniciando o jogo
+function start() {
+    $("#inicio").hide();
+    
+    $("#fundoGame").append("<div id='jogador'></div>");
+    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='inimigo2'></div>");
+    $("#fundoGame").append("<div id='amigo'></div>");
+}
