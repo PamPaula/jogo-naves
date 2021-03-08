c59d88e50f5924b9eaa33310598286a584a76c40
PamPaula <pamela.dpaulas@gmail.com>
2021-03-08 20:12:03 -0300

    criando loop e a movimentação de fundo

--- script.js
+++ script.js
@@ -6,4 +6,21 @@ function start() {
     $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
-}
+
+    //principais variaveis
+    var jogo = {}
+
+    //loop do jogo
+    jogo.timer = setInterval(loop,30);
+
+    function loop() {
+        movefundo();
+    } //fim da função loop
+
+    //movimento do fundo
+    function movefundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda-1);
+    } //fim do movimento
+
+} //fim do jogo
